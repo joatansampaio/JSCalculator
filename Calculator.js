84e0398437a482d6ec0f793ab98bc55c5c431569
joatansampaio <76176945+joatansampaio@users.noreply.github.com>
2021-08-19 17:14:22 -0500

    update

--- Calculator.js
+++ Calculator.js
@@ -1,36 +1,14 @@
-const one = document.getElementById("one");
-const two = document.getElementById("two");
-const three = document.getElementById("three");
-const four = document.getElementById("four");
-const five = document.getElementById("five");
-const six = document.getElementById("six");
-const seven = document.getElementById("seven");
-const eight = document.getElementById("eight");
-const nine = document.getElementById("nine");
-const zero = document.getElementById("zero");
-const sum = document.getElementById("sum");
-const sub = document.getElementById("sub");
-const mult = document.getElementById("mult");
-const division = document.getElementById("division");
-const percent = document.getElementById("percent");
-const sqrt = document.getElementById("sqrt");
-const dot = document.getElementById("dot");
-const clear = document.getElementById("clear");
-const display = document.getElementById("display");
+const numberButtons = document.querySelectorAll("[number-data]");
+const operatorButton = document.querySelectorAll("[operator-button]");
+const display = document.getElementById("display-box");
+const start = document.querySelector("#startValue");
 
-one.addEventListener("click", eventClick);
-
-function eventClick() {
-  display.innerText += "1";
-  alert("ola");
-  return 0;
-}
-
-//
-function createEvent(x) {
-  x.addEventListener("click", function () {
-    let aux = document.querySelector("#display");
-    aux.innerText = x.value;
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    display.innerText += button.innerText;
   });
+});
+
+function updateDisplay(update) {
+  display.innerText += update;
 }
-createEvent(one);
